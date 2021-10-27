f5128ad116eac507a14c325ccdd5506f4bc0f868
AngelDX <angel.condori@gmail.com>
2021-10-27 15:30:34 -0500

    parte 1 listar

---
+++ apirest/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Cliente
+
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombres', 'apellidos', 'dni', 'celular', 'email')
+
+admin.site.register(Cliente,ClienteAdmin)
+
